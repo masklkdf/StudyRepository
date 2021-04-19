4fb237319cfdc09f21b4bbb0c56a661f8947a06e
seekers <seekersMan@qq.com>
2021-04-19 23:49:21 +0800

    update

--- vue学习/vue的变化侦测/Observe.js
+++ vue学习/vue的变化侦测/Observe.js
@@ -1,10 +1,17 @@
+import { arrayMethods } from "./ArrayMethods";
+
+const hasProto = '__proto__' in {};
+const arrayKeys = Object.getOwnPropertyNames(arrayMethods);
+
 export default class Observe{
   constructor(value) {
     this.value = value;
 
     // 当为对象时，使用 get/set 方法
     if (Array.isArray(value)) {
-      value.__proto__ = arrayMethods;
+      const augment = hasProto ? protoAugment : copyAugment;
+      augment(value, arrayMethods, arrayKeys);
+      // value.__proto__ = arrayMethods;
     } else {
       this.walk(value);
     }
@@ -14,5 +21,15 @@ export default class Observe{
     for (let [key, value] of Object.entries(obj)) {
       defineReactive(obj, key, value);
     }
+  }
+}
+
+function protoAugment(target, src, arrayKeys) {
+  target.__proto__ = src;
+}
+
+function copyAugment(target, src, arrayKeys) {
+  for (let item of arrayKeys) {
+    target[item] = src[item];
   }
 }
